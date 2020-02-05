d026b22179048965b0a9e3f0c526109dbfff198e
VitakSharper <v.banard@gmail.com>
2020-02-05 17:19:28 +0100

    use dotnet cli to migrations
    -- >dotnet ef migrations add AddedValidation --project ../EFDataAccessLibrary

--- EFDataAccessLibrary/Models/Email.cs
+++ EFDataAccessLibrary/Models/Email.cs
@@ -1,8 +1,14 @@
-﻿namespace EFDataAccessLibrary.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFDataAccessLibrary.Models
 {
     public class Email
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(200)]
+        [Column(TypeName = "varchar(10)")]
         public string EmailAddress { get; set; }
     }
 }
